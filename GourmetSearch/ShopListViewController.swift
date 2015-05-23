645740aff56ff128348d927b26a1c3f8148fdbc7
shun3475 <imokenpi3475@gmail.com>
2015-05-23 19:53:37 +0900

    rename ViewController

---
+++ GourmetSearch/ShopListViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GourmetSearch
+//
+//  Created by 岩瀬　駿 on 2015/05/23.
+//  Copyright (c) 2015年 岩瀬　駿. All rights reserved.
+//
+
+import UIKit
+
+class ShopListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
