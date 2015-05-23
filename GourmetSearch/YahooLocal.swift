f81ee6c6e65f38a5d7f7b82183d307b2c01a0a2b
shun3475 <imokenpi3475@gmail.com>
2015-05-24 00:23:17 +0900

    install YahooLocallSearch Class

--- GourmetSearch/YahooLocal.swift
+++ GourmetSearch/YahooLocal.swift
@@ -119,7 +119,7 @@ let env = NSProcessInfo.processInfo().environment
 
 public class YahooLocalSearch{
     // Yahoo!ローカルサーチAPIのアプリケーションID
-    let apiID = env["apiID"] as? String
+    let apiId = env["apiID"] as? String
     
     // APIのベースURL
     let apiUrl = "http://search.olp.yahooapis.jp/OpenLocalPlatform/V1/localSearch"
@@ -144,4 +144,100 @@ public class YahooLocalSearch{
     
     // パラメタなしのイニシャライザ
     public init(){}
+    
+    // 検索条件をパラメタとして持つイニシャライザ
+    public init(condition: QueryCondition){ self.condition = condition }
+    
+    // APIからデータを読み込む
+    // reset = trueならデータを捨てて最初から読み込む
+    public func loadData(reset: Bool = false){
+        // reset = true なら今までの結果を捨てる
+        if reset {
+            shops = []
+            total = 0
+        }
+        
+        // 条件ディクショナリを取得
+        var params = condition.queryParams
+        
+        params["apiid"] = apiId
+        params["output"] = "json"
+        params["start"] = String(shops.count + 1)
+        params["result"] = String(perPage)
+        
+        // APIリクエスト実行
+        Alamofire.request(.GET, apiUrl, parameters: params).responseSwiftyJSON {
+            (request, reaponse, json, error) -> Void in
+            
+            // エラーがあれば終了
+            if error != nil {
+                return
+            }
+            
+            //店舗データをself.shopsに追加する
+            for (key, item) in json["Feature"] {
+                var shop = Shop()
+                
+                // 店舗ID
+                shop.gid = item["Gid"].string
+                
+                // 店舗名
+                var name = item["Name"].string
+                shop.name = name?.stringByReplacingOccurrencesOfString("&#39;", withString: "'", options: .LiteralSearch, range: nil)
+                
+                // 読み
+                shop.yomi = item["Property"]["Yomi"].string
+                
+                // 電話
+                shop.tel = item["Property"]["Tel1"].string
+                
+                //住所
+                shop.address = item["Property"]["Address"].string
+                
+                // 緯度&経度
+                if let geometry = item["Geometry"]["Coordinates"].string {
+                    let components = geometry.componentsSeparatedByString(",")
+                    // 緯度
+                    shop.lat = (components[1] as NSString).doubleValue
+                    // 経度
+                    shop.lon = (components[0] as NSString).doubleValue
+                }
+                
+                // キャッチコピー
+                shop.catchCopy = item["Property"]["CatchCopy"].string
+                
+                // 店舗写真
+                shop.photoUrl = item["Property"]["LoadImage"].string
+                
+                // クーポン有無
+                if item["Property"]["CouponFlag"].string == "true" {
+                    shop.hasCoupon = true
+                }
+                
+                // 駅
+                if let stations = item["Property"]["Station"].array {
+                    var line = ""
+                    if let lineString = stations[0]["Railway"].string {
+                        let lines = lineString.componentsSeparatedByString("/")
+                        line = lines[0]
+                    }
+                    
+                    if let station = stations[0]["Name"].string {
+                        // 駅名と路線名があれば両方入れる
+                        shop.station = "\(line) \(station)"
+                    } else {
+                        shop.station = "\(line)"
+                    }
+                }
+                
+                self.shops.append(shop)
+            }
+            // 総件数を反映
+            if let total = json["ResultInfo"]["Total"].int {
+                self.total = total
+            } else {
+                self.total = 0
+            }
+        }
+    }
 }
