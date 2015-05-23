653d1337b3bb7f0ab61edaa47007bb2fdc1bfbfe
shun3475 <imokenpi3475@gmail.com>
2015-05-23 21:07:13 +0900

    add tableViewCell attrs

---
+++ GourmetSearch/ShopListItemTableViewCell.swift
@@ -0,0 +1,34 @@
+//
+//  ShopListItemTableViewCell.swift
+//  GourmetSearch
+//
+//  Created by 岩瀬　駿 on 2015/05/23.
+//  Copyright (c) 2015年 岩瀬　駿. All rights reserved.
+//
+
+import UIKit
+
+class ShopListItemTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var photo: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var iconContainer: UIView!
+    @IBOutlet weak var coupon: UILabel!
+    @IBOutlet weak var station: UILabel!
+
+    @IBOutlet weak var nameHeight: NSLayoutConstraint!
+    @IBOutlet weak var stationWidth: NSLayoutConstraint!
+    @IBOutlet weak var stationX: NSLayoutConstraint!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
