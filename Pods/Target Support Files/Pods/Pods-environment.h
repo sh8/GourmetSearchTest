f2a5418612a366ef05d397e902eee6d8f8fc284b
shun3475 <imokenpi3475@gmail.com>
2015-06-11 14:46:26 +0900

    他のファイルの更新

--- Pods/Target Support Files/Pods/Pods-environment.h
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -6,6 +6,18 @@
 // project.
 
 
+// Alamofire
+#define COCOAPODS_POD_AVAILABLE_Alamofire
+#define COCOAPODS_VERSION_MAJOR_Alamofire 1
+#define COCOAPODS_VERSION_MINOR_Alamofire 2
+#define COCOAPODS_VERSION_PATCH_Alamofire 2
+
+// Alamofire-SwiftyJSON
+#define COCOAPODS_POD_AVAILABLE_Alamofire_SwiftyJSON
+#define COCOAPODS_VERSION_MAJOR_Alamofire_SwiftyJSON 1
+#define COCOAPODS_VERSION_MINOR_Alamofire_SwiftyJSON 1
+#define COCOAPODS_VERSION_PATCH_Alamofire_SwiftyJSON 0
+
 // SDWebImage
 #define COCOAPODS_POD_AVAILABLE_SDWebImage
 #define COCOAPODS_VERSION_MAJOR_SDWebImage 3
@@ -17,4 +29,10 @@
 #define COCOAPODS_VERSION_MAJOR_SDWebImage_Core 3
 #define COCOAPODS_VERSION_MINOR_SDWebImage_Core 7
 #define COCOAPODS_VERSION_PATCH_SDWebImage_Core 2
+
+// SwiftyJSON
+#define COCOAPODS_POD_AVAILABLE_SwiftyJSON
+#define COCOAPODS_VERSION_MAJOR_SwiftyJSON 2
+#define COCOAPODS_VERSION_MINOR_SwiftyJSON 2
+#define COCOAPODS_VERSION_PATCH_SwiftyJSON 0
 
