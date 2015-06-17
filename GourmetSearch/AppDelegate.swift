055d59ff8e407d36cd57e221ca2a5b958b700149
shun3475 <imokenpi3475@gmail.com>
2015-06-17 17:21:42 +0900

    お気に入り追加機能を実装

--- GourmetSearch/AppDelegate.swift
+++ GourmetSearch/AppDelegate.swift
@@ -15,7 +15,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        // お気に入りを読み込む
+        Favorite.load()
+        
         return true
     }
 
