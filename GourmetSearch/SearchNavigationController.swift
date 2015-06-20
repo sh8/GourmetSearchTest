6e2b9f67af66ded60d9b7f8c8f6c24e4e5931f0a
shun3475 <imokenpi3475@gmail.com>
2015-06-20 22:07:31 +0900

    create SearchNavigationController

---
+++ GourmetSearch/SearchNavigationController.swift
@@ -0,0 +1,35 @@
+//
+//  SearchNavigationController.swift
+//  GourmetSearch
+//
+//  Created by 岩瀬　駿 on 2015/06/17.
+//  Copyright (c) 2015年 岩瀬　駿. All rights reserved.
+//
+
+import UIKit
+
+class SearchNavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
