5f0d91a2ba282a84c7b4209619e283b9063a466d
shun3475 <imokenpi3475@gmail.com>
2015-06-21 16:05:07 +0900

    現在地付近のお店を取得する機能を実装

--- GourmetSearch/SearchTopTableViewController.swift
+++ GourmetSearch/SearchTopTableViewController.swift
@@ -7,19 +7,110 @@
 //
 
 import UIKit
+import CoreLocation
 
 class SearchTopTableViewController: UITableViewController, UITextFieldDelegate, UIGestureRecognizerDelegate {
     
     var freeword: UITextField? = nil
+    @IBOutlet var recognizer: UITapGestureRecognizer!
+
+    let ls = LocationService()
+    let nc = NSNotificationCenter.defaultCenter()
+    var observers = [NSObjectProtocol]()
+    var here: (lat: Double, lon: Double)? = nil
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.recognizer.delegate = self
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        // 位置情報取得を禁止している場合
+        observers.append(
+            nc.addObserverForName(
+                ls.LSAuthDeniedNotification,
+                object: nil,
+                queue: nil,
+                usingBlock: {
+                    notification in
+                    //位置情報がONになっていないダイアログ表示
+                    self.presentViewController(self.ls.locationServiceDisabledAlert, animated: true, completion: nil)
+                }
+            )
+        )
+        
+        // 位置情報取得を制限している場合
+        observers.append(
+            nc.addObserverForName(
+                ls.LSAuthDeniedNotification,
+                object: nil,
+                queue: nil,
+                usingBlock: {
+                    notification in
+                    //位置情報が制限されているダイアログ表示
+                    self.presentViewController(self.ls.locationServiceRestrictedAlert, animated: true, completion: nil)
+                }
+            )
+        )
+        
+        // 位置情報取得に失敗した場合
+        observers.append(
+            nc.addObserverForName(
+                ls.LSAuthDeniedNotification,
+                object: nil,
+                queue: nil,
+                usingBlock: {
+                    notification in
+                    //位置情報取得に失敗したダイアログ
+                    self.presentViewController(self.ls.locationServiceDidFailAlert, animated: true, completion: nil)
+                }
+            )
+        )
+        
+        // 位置情報を取得した場合
+        observers.append(
+            nc.addObserverForName(
+                ls.LSDidUpdateLocationNotification,
+                object: nil,
+                queue: nil,
+                usingBlock: {
+                    notification in
+                    if let userInfo = notification.userInfo as? [String: CLLocation] {
+                        if let clloc = userInfo["location"]{
+                            self.here = (lat: clloc.coordinate.latitude, lon: clloc.coordinate.longitude)
+                            self.performSegueWithIdentifier("PushShopListFromHere", sender: self)
+                        }
+                    }
+                }
+            )
+        )
+        
+        // 位置情報が利用可能になったとき
+        observers.append(
+            nc.addObserverForName(
+                ls.LSAuthorizedNotification,
+                object: nil,
+                queue: nil,
+                usingBlock: {
+                    notification in
+                }
+            )
+        )
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        // Notificationの待受を解除する
+        for observer in observers {
+            nc.removeObserver(observer)
+        }
+        
+        observers = []
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,6 +119,13 @@ class SearchTopTableViewController: UITableViewController, UITextFieldDelegate, 
     }
     
     // MARK: - UITableViewDelegate
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        if indexPath.section == 0 && indexPath.row == 1 {
+            ls.startUpdatingLocation()
+            tableView.deselectRowAtIndexPath(indexPath, animated: true)
+        }
+    }
+    
     override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
         return 44
     }
@@ -85,6 +183,12 @@ class SearchTopTableViewController: UITableViewController, UITextFieldDelegate, 
         if segue.identifier == "PushShopList" {
             let vc = segue.destinationViewController as! ShopListViewController
             vc.yls.condition.query = freeword?.text
+        }
+        
+        if segue.identifier == "PushShopListFromHere" {
+            let vc = segue.destinationViewController as! ShopListViewController
+            vc.yls.condition.lat = self.here?.lat
+            vc.yls.condition.lon = self.here?.lon
         }
     }
     
